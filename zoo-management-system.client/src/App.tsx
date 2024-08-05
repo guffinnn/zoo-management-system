cf4aa5dc31211bd5c06d431a49c6f4cc978ac5fd
Daniil <danidashkevichit@gmail.com>
2024-08-05 20:04:10 +0300

    feat: change routes in App

--- zoo-management-system.client/src/App.tsx
+++ zoo-management-system.client/src/App.tsx
@@ -1,30 +1,31 @@
-import { JSX } from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-/*REDUX*/
-import { useSelector } from "react-redux"
-import { RootState } from "./store/store.ts"
-import AsideNavBar from "./components/AsideNavBar/AsideNavBar.tsx"
-import Home from "./pages/Home/Home"
-import Animals from "./pages/Animals/Animals"
+import AsideNavBar from '@components/AsideNavBar/AsideNavBar';
+import { PATH } from '@constants/paths';
+import Animals from '@pages/Animals/Animals';
+import Home from '@pages/Home/Home';
+import { RootState } from '@store/store';
+import { JSX } from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App(): JSX.Element {
-    const isOpen = useSelector((state: RootState) => state.menu.isOpen)
+  const { TO_HOME, TO_ANIMALS } = PATH;
+  const isOpen = useSelector((state: RootState) => state.menu.isOpen);
 
-    return (
-        <>
-            {/*ASIDE COMPONENT*/}
-            <AsideNavBar />
-            {/*MAIN COMPONENT*/}
-            <main className={`main ${isOpen ? 'open' : ''}`}>
-                <Router>
-                    <Routes>
-                        <Route path="/zoo-management-system" element={<Home />} />
-                        <Route path="/zoo-management-system/animals" element={<Animals />} />
-                    </Routes>
-                </Router>
-            </main>
-        </>
-    )
+  return (
+    <>
+      {/*ASIDE COMPONENT*/}
+      <AsideNavBar />
+      {/*MAIN COMPONENT*/}
+      <main className={`main ${isOpen ? 'open' : ''}`}>
+        <Router>
+          <Routes>
+            <Route path={TO_HOME} element={<Home />} />
+            <Route path={TO_ANIMALS} element={<Animals />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
