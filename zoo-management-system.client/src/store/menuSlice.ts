10a89fcf10d6d9396ee6f03b2137d421df1ad220
Daniil <danidashkevichit@gmail.com>
2024-08-07 19:46:06 +0300

    feat: update menu context

--- zoo-management-system.client/src/store/menuSlice.ts
+++ zoo-management-system.client/src/store/menuSlice.ts
@@ -2,21 +2,31 @@ import { createSlice } from '@reduxjs/toolkit';
 
 interface MenuState {
   isOpen: boolean;
+  isClosing: boolean;
 }
 
 const initialState: MenuState = {
   isOpen: false,
+  isClosing: true,
 };
 
 const menuSlice = createSlice({
   name: 'menu',
   initialState,
   reducers: {
-    toggleMenu(state) {
-      state.isOpen = !state.isOpen;
+    openMenu(state) {
+      state.isOpen = true;
+      state.isClosing = false;
+    },
+    closeMenu(state) {
+      state.isClosing = true;
+    },
+    finishClosing(state) {
+      state.isOpen = false;
+      state.isClosing = false;
     },
   },
 });
 
-export const { toggleMenu } = menuSlice.actions;
+export const { openMenu, closeMenu, finishClosing } = menuSlice.actions;
 export default menuSlice.reducer;
