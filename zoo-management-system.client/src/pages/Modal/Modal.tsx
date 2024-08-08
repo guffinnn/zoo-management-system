2fc2368e277040d9062ec575b947e1013255f624
Daniil <danidashkevichit@gmail.com>
2024-08-08 20:42:26 +0300

    feat: add StatusModal & AuthModal, add routing to modals

---
+++ zoo-management-system.client/src/pages/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import AuthModal from '@components/modals/AuthModal/AuthModal.tsx';
+import StatusModal from '@components/modals/StatusModal/StatusModal.tsx';
+import { JSX, useEffect } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+
+function Modal(): JSX.Element {
+  const location = useLocation();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (location.pathname.includes('modal')) {
+      document.body.classList.add('--modal');
+    }
+    return () => {
+      document.body.classList.remove('--modal');
+    };
+  }, [location]);
+
+  return (
+    <>
+      {id === 'auth' && <AuthModal />}
+      {id === 'status' && <StatusModal />}
+    </>
+  );
+}
+
+export default Modal;
