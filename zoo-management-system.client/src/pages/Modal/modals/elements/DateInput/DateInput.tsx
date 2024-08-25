2009a15059032c147da691d1a1f594a28b6818fa
Daniil <danidashkevichit@gmail.com>
2024-08-25 12:17:17 +0300

    feat: update DateInput

--- zoo-management-system.client/src/pages/Modal/modals/elements/DateInput/DateInput.tsx
+++ zoo-management-system.client/src/pages/Modal/modals/elements/DateInput/DateInput.tsx
@@ -1,8 +1,11 @@
+import { FormikFields } from '@constants/tables/global.ts';
 import {
   StyledGroup,
   StyledGroupText,
   StyledInputWithLabel,
 } from '@pages/Modal/modals/AddDataModal/styled.ts';
+import { StyledInputWrapper } from '@pages/Modal/modals/elements/Input/styled.ts';
+import { useFormikContext } from 'formik';
 import { JSX } from 'react';
 
 interface DateInputProps {
@@ -16,10 +19,24 @@ export function DateInput({
   label,
   placeholder,
 }: DateInputProps): JSX.Element {
+  const { handleChange, handleBlur, values, touched, errors } =
+    useFormikContext<FormikFields>();
+  const isValidField = `${touched[id] && errors[id] ? 'error' : null}`;
+
   return (
-    <StyledGroup>
-      <StyledGroupText>{label}</StyledGroupText>
-      <StyledInputWithLabel type="date" id={id} placeholder={placeholder} />
-    </StyledGroup>
+    <StyledInputWrapper className={isValidField} error={`${errors[id]}`}>
+      <StyledGroup className={isValidField}>
+        <StyledGroupText>{label}</StyledGroupText>
+        <StyledInputWithLabel
+          type="date"
+          id={id}
+          name={id}
+          placeholder={placeholder}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[id]}
+        />
+      </StyledGroup>
+    </StyledInputWrapper>
   );
 }
