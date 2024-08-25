5cc9755eed7e492b4c6f923e70ba494856e68015
Daniil <danidashkevichit@gmail.com>
2024-08-25 12:19:03 +0300

    feat: update AddDataModal

--- zoo-management-system.client/src/pages/Modal/modals/AddDataModal/AddDataModal.tsx
+++ zoo-management-system.client/src/pages/Modal/modals/AddDataModal/AddDataModal.tsx
@@ -1,6 +1,8 @@
 import './AddDataModal.css';
 
-import { fieldConfig } from '@constants/tables/animal.ts';
+import { PATH } from '@constants/paths.ts';
+import { validationSchema } from '@constants/tables/animal.ts';
+import { fieldConfig, initialValues } from '@constants/tables/global.ts';
 import {
   FormFrame,
   ModalContainer,
@@ -11,21 +13,37 @@ import {
   SubmitButton,
 } from '@pages/Modal/modals/AuthModal/styled.ts';
 import { Element } from '@pages/Modal/modals/elements/Element.tsx';
+import { Formik } from 'formik';
 import { JSX } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function AddDataModal(): JSX.Element {
-  const fields = fieldConfig['workTime'];
+  const fields = fieldConfig['animal'];
+  const initial = initialValues['animal'];
+
+  const navigate = useNavigate();
 
   return (
     <ModalContainer>
       <ModalHeading>Заполните поля</ModalHeading>
       <FormFrame>
-        <Form>
-          {fields.map((field) => (
-            <Element key={field.id} element={field} />
-          ))}
-          <SubmitButton type="submit" value="Подтвердить действия" />
-        </Form>
+        <Formik
+          initialValues={initial}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            console.log(values);
+            navigate(PATH.TO_VALIDATION_MODAL);
+          }}
+        >
+          {({ handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              {fields.map((field) => (
+                <Element key={field.id} element={field} />
+              ))}
+              <SubmitButton type="submit" value="Подтвердить действия" />
+            </Form>
+          )}
+        </Formik>
       </FormFrame>
     </ModalContainer>
   );
