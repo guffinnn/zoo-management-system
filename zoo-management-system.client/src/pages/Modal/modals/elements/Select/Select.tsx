6bd8c0d96b3bf7eea91c29f4a8214422eba80ef1
Daniil <danidashkevichit@gmail.com>
2024-08-25 12:16:47 +0300

    feat: update Select

--- zoo-management-system.client/src/pages/Modal/modals/elements/Select/Select.tsx
+++ zoo-management-system.client/src/pages/Modal/modals/elements/Select/Select.tsx
@@ -1,8 +1,11 @@
+import { FormikFields } from '@constants/tables/global.ts';
 import {
   StyledGroup,
   StyledGroupText,
 } from '@pages/Modal/modals/AddDataModal/styled.ts';
+import { StyledInputWrapper } from '@pages/Modal/modals/elements/Input/styled.ts';
 import { StyledSelect } from '@pages/Modal/modals/elements/Select/styled.ts';
+import { useFormikContext } from 'formik';
 import { JSX } from 'react';
 
 interface SelectProps {
@@ -12,18 +15,28 @@ interface SelectProps {
 }
 
 export function Select({ id, label, options }: SelectProps): JSX.Element {
+  const { handleChange, handleBlur, values, touched, errors } =
+    useFormikContext<FormikFields>();
+  const isValidField = `${touched[id] && errors[id] ? 'error' : null}`;
+
   return (
-    <StyledGroup>
-      <StyledGroupText>{label}</StyledGroupText>
-      <StyledSelect id={id} className="form-control">
-        <option className="option" value="NULL">
+    <StyledInputWrapper className={isValidField} error={`${errors[id]}`}>
+      <StyledGroup className={isValidField}>
+        <StyledGroupText>{label}</StyledGroupText>
+        <StyledSelect
+          id={id}
+          name={id}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values[id]}
+        >
           {options.map((option) => (
             <option key={option.value} value={option.value}>
               {option.text}
             </option>
           ))}
-        </option>
-      </StyledSelect>
-    </StyledGroup>
+        </StyledSelect>
+      </StyledGroup>
+    </StyledInputWrapper>
   );
 }
