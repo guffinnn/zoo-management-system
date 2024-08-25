38a78749f71abe76f745c42f68c09aac93920a62
Daniil <danidashkevichit@gmail.com>
2024-08-25 12:16:20 +0300

    feat: update Input

--- zoo-management-system.client/src/pages/Modal/modals/elements/Input/Input.tsx
+++ zoo-management-system.client/src/pages/Modal/modals/elements/Input/Input.tsx
@@ -1,4 +1,9 @@
-import { StyledInput } from '@pages/Modal/modals/elements/Input/styled.ts';
+import { FormikFields } from '@constants/tables/global.ts';
+import {
+  StyledInput,
+  StyledInputWrapper,
+} from '@pages/Modal/modals/elements/Input/styled.ts';
+import { useFormikContext } from 'formik';
 import { JSX } from 'react';
 
 interface InputProps {
@@ -8,5 +13,22 @@ interface InputProps {
 }
 
 export function Input({ type, id, placeholder }: InputProps): JSX.Element {
-  return <StyledInput type={type} id={id} placeholder={placeholder} />;
+  const { handleChange, handleBlur, values, touched, errors } =
+    useFormikContext<FormikFields>();
+  const isValidField = `${touched[id] && errors[id] ? 'error' : null}`;
+
+  return (
+    <StyledInputWrapper className={isValidField} error={`${errors[id]}`}>
+      <StyledInput
+        type={type}
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values[id]}
+        className={isValidField}
+      />
+    </StyledInputWrapper>
+  );
 }
