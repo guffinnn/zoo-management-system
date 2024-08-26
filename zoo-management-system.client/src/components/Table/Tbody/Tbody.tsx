dc2a74591d419d49c0f41478823b70edc432de15
Daniil <danidashkevichit@gmail.com>
2024-08-26 18:48:16 +0300

    feat: update Tbody to use custom status field

--- zoo-management-system.client/src/components/Table/Tbody/Tbody.tsx
+++ zoo-management-system.client/src/components/Table/Tbody/Tbody.tsx
@@ -4,15 +4,30 @@ import { TableProps } from '@components/Table/Table';
 import { JSX } from 'react';
 
 function Tbody({ columns, data }: TableProps): JSX.Element {
+  const getStatus = (status: string) => {
+    switch (status) {
+      case 'Выполнено':
+        return <p className="cell__content select done">{status}</p>;
+      case 'В процессе':
+        return <p className="cell__content select in_work">{status}</p>;
+      case 'Назначено':
+        return <p className="cell__content select planned">{status}</p>;
+    }
+  };
+
   return (
     <>
       {data.map((entity, index) => (
         <tr key={index}>
           {Object.entries(entity).map(([field, value], i) => (
             <td key={i} className="cell">
-              <p className={`cell__content ${columns[i].type}`}>
-                {field === 'actions' ? <div className="delete"></div> : value}
-              </p>
+              {columns[i].name !== 'Статус' ? (
+                <p className={`cell__content ${columns[i].type}`}>
+                  {field === 'actions' ? <div className="delete"></div> : value}
+                </p>
+              ) : (
+                getStatus(value)
+              )}
             </td>
           ))}
         </tr>
