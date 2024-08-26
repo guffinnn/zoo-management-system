560794fcb82952f327f7d19da852855682618908
Daniil <danidashkevichit@gmail.com>
2024-08-26 14:49:22 +0300

    feat: update App

--- zoo-management-system.client/src/App.tsx
+++ zoo-management-system.client/src/App.tsx
@@ -3,11 +3,12 @@ import Animals from '@pages/Animals/Animals';
 import Feeding from '@pages/Feeding/Feeding.tsx';
 import Home from '@pages/Home/Home';
 import Modal from '@pages/Modal/Modal.tsx';
+import WorkTime from '@pages/WorkTime/WorkTime.tsx';
 import { JSX } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App(): JSX.Element {
-  const { TO_HOME, TO_ANIMALS, TO_FEEDING, TO_MODAL } = PATH;
+  const { TO_HOME, TO_ANIMALS, TO_FEEDING, TO_WORKTIME, TO_MODAL } = PATH;
 
   return (
     <Router>
@@ -15,6 +16,7 @@ function App(): JSX.Element {
         <Route path={TO_HOME} element={<Home />} />
         <Route path={TO_ANIMALS} element={<Animals />} />
         <Route path={TO_FEEDING} element={<Feeding />} />
+        <Route path={TO_WORKTIME} element={<WorkTime />} />
         <Route path={TO_MODAL} element={<Modal />} />
       </Routes>
     </Router>
