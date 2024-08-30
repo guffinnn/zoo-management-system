3e4d25f63f2e98d0e96018fea47e5def3c0d4709
Daniil <danidashkevichit@gmail.com>
2024-08-30 20:16:43 +0300

    feat: add firestore get methods & Loader component

---
+++ zoo-management-system.client/src/types/database/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id: string;
+  user_uid: string;
+  surname: string;
+  name: string;
+  middle_name: string;
+  role: string;
+  salary: number;
+  date_of_hire: string;
+}
---
+++ zoo-management-system.client/src/utils/dbService.ts
@@ -0,0 +1,66 @@
+import { Animal } from '@custom-types/database/animal.ts';
+import { WorkTime } from '@custom-types/database/workTime.ts';
+import { collection, getDoc, getDocs } from 'firebase/firestore';
+
+import { db } from '../firebase';
+
+let cachedAnimals: Animal[] | null = null;
+
+export async function getAnimals(): Promise<Animal[]> {
+  if (cachedAnimals) {
+    return cachedAnimals;
+  }
+
+  const querySnapshot = await getDocs(collection(db, 'animal'));
+  const animals = querySnapshot.docs.map((doc) => {
+    const data = doc.data();
+    return {
+      id: doc.id,
+      nickname: data.nickname,
+      species: data.species,
+      gender: data.gender,
+      date_of_birth: data.date_of_birth,
+      date_of_registration: data.date_of_registration,
+      photo: data.photo || 'Пусто',
+      actions: '',
+    };
+  });
+
+  cachedAnimals = animals;
+  return animals;
+}
+
+let cachedWorkTime: WorkTime[] | null = null;
+
+export async function getWorkTime(): Promise<WorkTime[]> {
+  if (cachedWorkTime) {
+    return cachedWorkTime;
+  }
+
+  const querySnapshot = await getDocs(collection(db, 'workTime'));
+  const workTime = await Promise.all(
+    querySnapshot.docs.map(async (doc) => {
+      const data = doc.data();
+      const employeeRef = data.employee_id;
+      const employeeDoc = await getDoc(employeeRef);
+      const employeeData: {
+        surname: string;
+        name: string;
+        middle_name: string;
+      } = employeeDoc.data();
+
+      return {
+        id: doc.id,
+        employee: `${employeeData.surname} ${employeeData.name} ${employeeData.middle_name}`,
+        date_of_work: data.date_of_work,
+        type_of_work: data.type_of_work || 'Неизвестно',
+        status: data.status,
+        time: data.time,
+        actions: '',
+      };
+    }),
+  );
+
+  cachedWorkTime = workTime;
+  return workTime;
+}
