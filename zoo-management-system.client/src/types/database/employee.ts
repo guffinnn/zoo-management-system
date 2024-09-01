7ded9055c80d1cb952eb5587757f804e566e79d2
Daniil <danidashkevichit@gmail.com>
2024-09-01 14:13:25 +0300

    feat: update employee type

--- zoo-management-system.client/src/types/database/employee.ts
+++ zoo-management-system.client/src/types/database/employee.ts
@@ -1,10 +1,11 @@
 export interface Employee {
-  id: string;
-  user_uid: string;
-  surname: string;
+  id?: string;
+  user_uid?: string;
   name: string;
+  surname: string;
   middle_name: string;
   role: string;
   salary: number;
   date_of_hire: string;
+  actions?: string;
 }
