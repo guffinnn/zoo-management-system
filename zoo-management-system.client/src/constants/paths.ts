815d068e2df56adad6d1da44fab097f771ce67e4
Daniil <danidashkevichit@gmail.com>
2024-09-02 18:08:53 +0300

    feat: add new constants for MedicalExam

--- zoo-management-system.client/src/constants/paths.ts
+++ zoo-management-system.client/src/constants/paths.ts
@@ -3,15 +3,18 @@ export const PATH = {
   TO_ANIMALS: '/animals',
   TO_FEEDING: '/feeding',
   TO_WORKTIME: '/worktime',
+  TO_EXAMINATION: '/medicalExamination',
   TO_MODAL: '/modal/:modalType/:dataType',
   TO_AUTH_MODAL: '/modal/auth/1',
   TO_STATUS_MODAL: '/modal/status/1',
   TO_ADD_MODAL: {
     animal: '/modal/add/animal',
     workTime: '/modal/add/workTime',
+    medicalExamination: '/modal/add/medicalExamination',
   },
   TO_VALIDATION_MODAL: {
     animal: '/modal/validation/animal',
     workTime: '/modal/validation/workTime',
+    medicalExamination: '/modal/validation/medicalExamination',
   },
 };
