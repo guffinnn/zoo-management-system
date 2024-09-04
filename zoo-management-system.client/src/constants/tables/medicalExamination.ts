8d6d3a8d73bb0840a80f8d359ebd2529b2f8314b
Daniil <danidashkevichit@gmail.com>
2024-09-04 10:57:54 +0300

    feat: create validation schema

--- zoo-management-system.client/src/constants/tables/medicalExamination.ts
+++ zoo-management-system.client/src/constants/tables/medicalExamination.ts
@@ -1,4 +1,6 @@
+import { DEFAULT_MIN_CHARACTERS, ERROR } from '@constants/values.ts';
 import { Column } from '@custom-types/database/animal.ts';
+import * as Yup from 'yup';
 
 export const MEDICAL_EXAMINATION_COLUMNS: Column[] = [
   { name: 'Животное', type: 'text' },
@@ -6,3 +8,15 @@ export const MEDICAL_EXAMINATION_COLUMNS: Column[] = [
   { name: 'Заключение', type: 'document' },
   { name: 'Действия', type: 'edit' },
 ];
+
+const currentDate = new Date();
+
+export const validationSchema = Yup.object().shape({
+  animal: Yup.string().required(ERROR.REQUIRED),
+  date_of_examination: Yup.date()
+    .required(ERROR.REQUIRED)
+    .max(currentDate, ERROR.DATE_TOO_LATE),
+  notes: Yup.string()
+    .required(ERROR.REQUIRED)
+    .min(DEFAULT_MIN_CHARACTERS, ERROR.NOTE_LENGTH_IS_LESS),
+});
