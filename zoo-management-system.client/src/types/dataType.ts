ce49c01995fbc4699e58fe1ec4354cd805dce16e
Daniil <danidashkevichit@gmail.com>
2024-09-15 19:20:14 +0300

    feat: update Table

--- zoo-management-system.client/src/types/dataType.ts
+++ zoo-management-system.client/src/types/dataType.ts
@@ -1,5 +1,18 @@
+import { Animal } from '@custom-types/database/animal.ts';
+import { Employee } from '@custom-types/database/employee.ts';
+import { MedicalExamination } from '@custom-types/database/medicalExamination.ts';
+import { WorkTime } from '@custom-types/database/workTime.ts';
+
 export type DataType =
   | 'animal'
   | 'workTime'
   | 'medicalExamination'
   | 'employees';
+
+export type EntityData = Animal | WorkTime | MedicalExamination | Employee;
+
+export type TableData =
+  | Animal[]
+  | WorkTime[]
+  | MedicalExamination[]
+  | Employee[];
