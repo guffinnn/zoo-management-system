98aee821ded644453a46c95b451f13988a8336a1
Daniil <danidashkevichit@gmail.com>
2024-09-23 19:58:58 +0300

    fix: change ValidationModal for use new correct messages

--- zoo-management-system.client/src/pages/Modal/modals/ValidationModal/ValidationModal.tsx
+++ zoo-management-system.client/src/pages/Modal/modals/ValidationModal/ValidationModal.tsx
@@ -3,8 +3,6 @@ import success from '@assets/success.svg';
 import { PATH } from '@constants/paths.ts';
 import {
   BUTTON_ADD,
-  BUTTON_DONT_ADD,
-  BUTTON_EXIT,
   BUTTON_RETRY,
   STATUS_CONFIRMED,
   STATUS_CONFIRMED_INFO,
@@ -24,50 +22,38 @@ import {
 } from '@pages/Modal/modals/StatusModal/styled.ts';
 import { Button } from '@pages/Modal/modals/ValidationModal/styled.ts';
 import { JSX } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 function ValidationModal(): JSX.Element {
-  const status = true;
+  const location = useLocation();
+  const status = location.state?.status ?? false;
   const { dataType } = useParams<{ dataType: DataType }>();
   const navigate = useNavigate();
 
-  const handleExitClick = () => {
-    if (!status) {
-      // TODO: Add data to database
-    }
-    navigate(PATH.TO_HOME);
-  };
-
   const handleTryAgainClick = () => {
-    const TO_MODAL = PATH.TO_ADD_MODAL;
-    navigate(TO_MODAL[dataType!]);
+    navigate(PATH.TO_HOME);
   };
 
   return (
     <ModalContainer>
       <InfoContent>
-        <StatusImageContainer className={status ? 'error' : 'success'}>
-          <StatusImage src={status ? error : success} />
+        <StatusImageContainer className={!status ? 'error' : 'success'}>
+          <StatusImage src={!status ? error : success} />
         </StatusImageContainer>
         <ModalContent>
           <StatusHeading>
-            {status ? STATUS_REJECTED : STATUS_CONFIRMED}
+            {!status ? STATUS_REJECTED : STATUS_CONFIRMED}
           </StatusHeading>
           <StatusInfo>
-            {status ? STATUS_REJECTED_INFO : STATUS_CONFIRMED_INFO[dataType!]}
+            {!status ? STATUS_REJECTED_INFO : STATUS_CONFIRMED_INFO[dataType!]}
           </StatusInfo>
         </ModalContent>
         <ButtonContainer>
           <Button
             onClick={handleTryAgainClick}
-            className={status ? 'error' : 'success'}
+            className={!status ? 'error' : 'success'}
           >
-            {status ? BUTTON_RETRY : BUTTON_ADD}
-          </Button>
-        </ButtonContainer>
-        <ButtonContainer>
-          <Button onClick={handleExitClick}>
-            {status ? BUTTON_EXIT : BUTTON_DONT_ADD}
+            {!status ? BUTTON_RETRY : BUTTON_ADD}
           </Button>
         </ButtonContainer>
       </InfoContent>
