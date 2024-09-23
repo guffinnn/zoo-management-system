75fca99a11aa124d40453a3e7904263e4da9f799
Daniil <danidashkevichit@gmail.com>
2024-09-23 19:49:42 +0300

    feat: add dataPushers for connect realizations

--- zoo-management-system.client/src/helpers/addDataModalHelpers.ts
+++ zoo-management-system.client/src/helpers/addDataModalHelpers.ts
@@ -1,10 +1,20 @@
-import { DataType, EntityData } from '@custom-types/dataType.ts';
+import {
+  DataType,
+  EntityData,
+  EntityDataUnion,
+} from '@custom-types/dataType.ts';
 import {
   getAnimalById,
   getEmployeeById,
   getExaminationById,
   getWorkTimeById,
 } from '@utils/dbService.ts';
+import {
+  putAnimal,
+  putEmployee,
+  putExamination,
+  putWorkTime,
+} from '@utils/putService.ts';
 
 export const dataFetchers: {
   [key in DataType]: (id: string) => Promise<EntityData | null>;
@@ -13,4 +23,13 @@ export const dataFetchers: {
   workTime: getWorkTimeById,
   medicalExamination: getExaminationById,
   employees: getEmployeeById,
+};
+
+export const dataPushers: {
+  [key in DataType]: (param: EntityDataUnion) => Promise<void>;
+} = {
+  animal: putAnimal,
+  workTime: putWorkTime,
+  medicalExamination: putExamination,
+  employees: putEmployee,
 };
